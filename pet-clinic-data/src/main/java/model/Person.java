802e3f1a3b225748bfc1bcf6dc47ec69e3f921ca
Mateusz Slawniewicz <mateusz.slawniewicz@gmail.com>
2019-04-19 17:57:58 +0200

    refactoring to common crud interface. Closes #10.

--- pet-clinic-data/src/main/java/model/Person.java
+++ pet-clinic-data/src/main/java/model/Person.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class Person {
-    private String firstName;
+public class Person extends BaseEntity{
+        private String firstName;
     private String lastName;
 }
