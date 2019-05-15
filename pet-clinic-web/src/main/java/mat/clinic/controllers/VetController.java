745b80300880dfe6b16374e2d3be054d7738468e
Mateusz Slawniewicz <mateusz.slawniewicz@gmail.com>
2019-05-16 00:28:59 +0200

    apply master layout to vets index page. Closes #19

--- pet-clinic-web/src/main/java/mat/clinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/mat/clinic/controllers/VetController.java
@@ -1,12 +1,21 @@
 package mat.clinic.controllers;
 
+import mat.clinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+
+    @Autowired
+    private VetService vetService;
+
+
     @RequestMapping({"/vets", "vets/index", "vets/index/html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
