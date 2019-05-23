fa0e9279cfd75eae8e1ecf5cabda819a2ed0ae86
Mateusz Slawniewicz <mateusz.slawniewicz@gmail.com>
2019-05-23 17:10:28 +0200

    owners converted to JPA entities. Closes #31

--- pet-clinic-data/src/main/java/mat/clinic/model/PetType.java
+++ pet-clinic-data/src/main/java/mat/clinic/model/PetType.java
@@ -3,8 +3,15 @@ package mat.clinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @Getter
 @Setter
-public class PetType extends BaseEntity{
+@Entity
+@Table(name = "types")
+public class PetType extends BaseEntity {
+    @Column(name = "name")
     private String name;
 }
