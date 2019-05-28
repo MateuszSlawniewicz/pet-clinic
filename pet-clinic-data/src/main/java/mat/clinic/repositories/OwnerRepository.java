e6df2fe1d06277d0ea2a214df93cae5e5fa0b155
Mateusz Slawniewicz <mateusz.slawniewicz@gmail.com>
2019-05-28 13:01:05 +0200

    created Spring Data JPA service for Owners. Closes #35

--- pet-clinic-data/src/main/java/mat/clinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/mat/clinic/repositories/OwnerRepository.java
@@ -4,4 +4,5 @@ import mat.clinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
